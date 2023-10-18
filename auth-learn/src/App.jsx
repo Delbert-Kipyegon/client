03b17c0a2b5cc61761a54f25b4e724c6651df81e
Delbert-Kipyegon <delbertkip@gmail.com>
2023-10-18 18:56:45 +0300

    demo

--- auth-learn/src/App.jsx
+++ auth-learn/src/App.jsx
@@ -4,7 +4,12 @@ import Homepage from "./pages/Homepage";
 export default function App() {
   return (
    <>
-   <Homepage />   
+   <Homepage /> 
+   {/* <Homepage /> 
+   <Homepage /> 
+   <Homepage /> 
+   <Homepage />  */}
+
    </>
   )
 }
